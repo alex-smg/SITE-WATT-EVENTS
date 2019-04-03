a0a7b899ccf5c693e4147129f6f2c4b64dcdd249
unknown <alex.s95120@gmail.com>
2019-04-03 10:48:40 +0200

    add seminaire

---
+++ src/index.php
@@ -0,0 +1,440 @@
+<!doctype html>
+<html class="no-js" lang="FR">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>WATT EVENTS</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,800,900" rel="stylesheet">
+    <link rel="apple-touch-icon" href="icon.png">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+<header id="nav">
+
+    <nav>
+        <div>
+            <img src="img/logo-watt.png">
+
+        </div>
+
+        <ul>
+            <li><a href="#first_article">
+                <div class="circle"></div>
+                Agence</a></li>
+            <li><a href="#">
+                <div class="circle"></div>
+                Projets</a></li>
+            <li><a href="#">
+                <div class="circle"></div>
+                Contacts</a></li>
+        </ul>
+
+    </nav>
+
+</header>
+
+<main>
+    <img id="fond-nav" src="img/fond_2.svg">
+    <section>
+        <div>
+            <img src="img/logo-home-04-04.svg" alt="">
+            <a id="decouvrir" href="#first-article">Découvrir</a>
+        </div>
+
+    </section>
+
+
+    <article id="first_article">
+        <div id="content-fa" class="content-fa" role="contentinfo">
+            <div id="text-slide-left" class="content-fa-text slide-left">
+                <h2>
+                    <div class="circle"></div>
+                    Nos métiers
+                </h2>
+                <div class="circle circle-fa"></div>
+                <p>
+                    Tout né d'une idée, c'est la base de notre travail, l'idée de raconté une histoire à vos invités
+                    grace à un concept créatif et innovant selon vos envies.<br> Accompagner par notre réseau, Nous vous
+                    proposons une offre complète qui intègre : <br>
+                <ul class="list-first-article">
+                    <li>- Un lieu prompt à accueillir votre public dans les meilleures conditions.</li>
+                    <li>- Une restauration de qualité personnalisée selon le thème de l’évènement.</li>
+                    <li>- Une prestation technique définie en fonction du lieu et du concept de votre manifestation.
+                    </li>
+                    <li>- Une décoration adaptée à la thématique de votre évènement.</li>
+                    <li>- La dynamisation artistique illustrant les valeurs et messages que vous souhaitez
+                        transmettre.
+                    </li>
+                    <li>- Notre équipe d’encadrement, qui veille au bon déroulement de votre opération depuis sa
+                        conception jusqu’à sa réalisation.
+                    </li>
+                </ul>
+
+                </p></div>
+
+            <p>
+                <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 1074 768" style="enable-background:new 0 0 1074 768;" xml:space="preserve">
+<style type="text/css">
+	.st0 {
+        fill: url(#SVGID_1_);
+    }
+
+    .st1 {
+        fill: url(#SVGID_2_);
+    }
+
+    .st2 {
+        fill: url(#SVGID_3_);
+    }
+
+    .st3 {
+        fill: url(#SVGID_4_);
+    }
+
+    .st4 {
+        fill: url(#SVGID_5_);
+    }
+</style>
+<g>
+	<g id="XMLID_14_">
+		<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="7.8297" y1="674.1184" x2="905.177" y2="72.35">
+			<stop  offset="0" style="stop-color:#6B3C8F"/>
+			<stop  offset="1" style="stop-color:#654997"/>
+		</linearGradient>
+		<path class="st0" d="M1021.94,242.37c-4.74-83.59-71.99-150.88-155.58-155.68c-57.91-3.32-109.82,23.13-141.87,65.45
+			c-11.55,15.25-33.08,18.73-48.73,7.74c-26.9-18.9-59.68-29.99-95.06-29.99c-27.68,0-53.78,6.8-76.71,18.81
+			c-12.34,6.46-27.23,5.19-38.43-3.09c-27.49-20.31-61.49-32.32-98.3-32.32c-42.28,0-80.85,15.84-110.12,41.92
+			c-7.75,6.9-18.66,9.02-28.48,5.66c-16.9-5.77-35.02-8.88-53.88-8.84c-90.53,0.2-164.66,74.02-165.23,164.55
+			c-0.58,91.96,73.79,166.68,165.61,166.68c18.79,0,36.85-3.13,53.69-8.9c23.25-7.97,48.77,1.54,61.76,22.4
+			c14.84,23.84,35.58,43.63,60.17,57.34c6.96,3.88,11.46,10.96,12.33,18.87c9.05,82.92,79.3,147.44,164.62,147.44
+			c63.17,0,118.07-35.37,146-87.38c9.16-17.05,27.19-27.29,46.54-27.24c0.12,0,0.23,0,0.35,0c91.47,0,165.61-74.15,165.61-165.61
+			c0-8.45-0.64-16.75-1.86-24.86C965.67,401.57,1026.83,328.63,1021.94,242.37z"/>
+	</g>
+	<g>
+		<linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="54.0717" y1="743.0737" x2="951.419" y2="141.3053">
+			<stop  offset="0" style="stop-color:#6B3C8F"/>
+			<stop  offset="1" style="stop-color:#654997"/>
+		</linearGradient>
+		<circle class="st1" cx="220.45" cy="631.5" r="125"/>
+	</g>
+	<g>
+		<linearGradient id="SVGID_3_" gradientUnits="userSpaceOnUse" x1="74.8123" y1="774.0018" x2="972.1597" y2="172.2334">
+			<stop  offset="0" style="stop-color:#6B3C8F"/>
+			<stop  offset="1" style="stop-color:#654997"/>
+		</linearGradient>
+		<circle class="st2" cx="897.95" cy="222" r="176.5"/>
+	</g>
+	<g>
+		<linearGradient id="SVGID_4_" gradientUnits="userSpaceOnUse" x1="-201.8323" y1="361.4738" x2="695.5151" y2="-240.2946">
+			<stop  offset="0" style="stop-color:#6B3C8F"/>
+			<stop  offset="1" style="stop-color:#654997"/>
+		</linearGradient>
+		<circle class="st3" cx="251.45" cy="57.5" r="46"/>
+	</g>
+	<g>
+		<linearGradient id="SVGID_5_" gradientUnits="userSpaceOnUse" x1="222.6947" y1="994.5217" x2="1120.042" y2="392.7532">
+			<stop  offset="0" style="stop-color:#6B3C8F"/>
+			<stop  offset="1" style="stop-color:#654997"/>
+		</linearGradient>
+		<circle class="st4" cx="734.95" cy="651" r="28.5"/>
+	</g>
+</g>
+</svg>
+
+            </p>
+
+        </div>
+
+        <div id="content-fa-down" class="content-fa" role="contentinfo">
+
+            <div class="content-svg">
+                <svg id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 639 607.61">
+                    <defs>
+                        <style>.cls-1 {
+                            fill: #e2de00;
+                        }</style>
+                    </defs>
+                    <title>Sans titre - 3</title>
+                    <circle class="cls-1" cx="213.67" cy="514.66" r="35.01"/>
+                    <path class="cls-1"
+                          d="M1080.22,446.24a201,201,0,0,0-26.82,1.81,117.34,117.34,0,0,0,18-62.57c0-65.07-54.24-118.65-119.31-117.89a117.9,117.9,0,0,0-116.5,117.89c0,.82,0,1.63,0,2.45A160.55,160.55,0,1,0,865,694.76a19.81,19.81,0,0,1,26.25,12.09,198.88,198.88,0,0,0,188.61,137C1189.51,844,1279,754.68,1279,645S1190,446.24,1080.22,446.24Z"
+                          transform="translate(-640 -236.19)"/>
+                    <path class="cls-1"
+                          d="M1075.39,422.1a199.47,199.47,0,0,0-26.82,1.81,117.34,117.34,0,0,0,18-62.57c0-65.08-54.23-118.65-119.31-117.9a117.91,117.91,0,0,0-116.5,117.9c0,.82,0,1.63,0,2.45a160.55,160.55,0,1,0,29.39,306.82,19.8,19.8,0,0,1,26.25,12.1A198.87,198.87,0,0,0,1075,819.66c109.66.2,199.15-89.12,199.15-198.78S1185.17,422.1,1075.39,422.1Z"
+                          transform="translate(-640 -236.19)"/>
+                    <circle class="cls-1" cx="309.44" cy="122.73" r="122.73"/>
+                </svg>
+
+            </div>
+            <div id="text-slide-right" class="content-fa-text-right slide-right">
+                <h2>
+                    <div class="circle"></div>
+                    Notre Agence
+                </h2>
+
+                <div class="circle circle-fa"></div>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                    laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                    ullamcorper suscipit lobortis nisl Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                    diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat..
+                    </ul>
+
+                </p></div>
+
+
+        </div>
+    </article>
+
+
+    <article id="second-article">
+
+        <div class="cont-tab" role="group">
+            <div class="tab" role="row">
+                <div role="img">
+                    <img src="" alt="">
+                </div>
+                <div role="textbox">
+                    <p>
+                        <h3>Titre</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="tab" role="row">
+                <div role="img">
+                    <img src="" alt="">
+                </div>
+                <div role="textbox">
+                    <p>
+                    <h3>Titre</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </p>
+                </div>
+            </div>
+
+            <div class="tab" role="row">
+                <div role="img">
+                    <img src="" alt="">
+                </div>
+                <div role="textbox">
+                    <p>
+                    <h3>Titre</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </p>
+                </div>
+            </div>
+
+            </div>
+
+
+
+
+        <div class="cont-tab" role="group">
+            <div class="tab" role="row">
+                <div role="img">
+                    <img src="" alt="">
+                </div>
+                <div role="textbox">
+                    <p>
+                    <h3>Titre</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="tab" role="row">
+                <div role="img">
+                    <img src="" alt="">
+                </div>
+                <div role="textbox">
+                    <p>
+                    <h3>Titre</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </p>
+                </div>
+            </div>
+
+            <div class="tab" role="row">
+                <div role="img">
+                    <img src="" alt="">
+                </div>
+                <div role="textbox">
+                    <p>
+                    <h3>Titre</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+    </article>
+
+
+    <article id="thorth-article">
+
+        <div id="input-newsletter">
+
+            <div>
+
+
+
+                <h4 id="label" class="label">Abonnez vous pour recevoir toutes nos news</h4>
+                <input id="mail" type="email" placeholder="Votre adresse email">
+
+                <span id="border-input" class="border"></span>
+            </div>
+
+
+
+            <button class="btn" type="submit">Je m'abonne</button>
+
+
+
+        </div>
+
+
+
+
+
+
+
+    </article>
+
+    <article id="fourth">
+
+        <div id="map"></div>
+
+        <div id="form">
+            <form id="contact-form" action="POST">
+                <h4>Contactez nous</h4>
+
+                <input type="text" placeholder="Votre Nom">
+                <input type="text" placeholder="Votre prénom">
+                <input type="email" placeholder="Votre email">
+                <input type="text" placeholder="Sujet">
+                <textarea placeholder="Votre demande"></textarea>
+                <button class="btn" type="submit">Envoyer</button>
+
+
+
+            </form>
+
+
+        </div>
+
+
+
+    </article>
+
+    <!--<article>
+        <svg></svg>
+        <div>
+            <h3>Lorem ipsum dolor sit</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, aliquam asperiores cum distinctio eum,
+                facere natus officia placeat, quia rem reprehenderit saepe sint veniam. Consequatur earum fuga itaque
+                laudantium reiciendis!</p>
+            <div>
+                <div>
+                    <button>conférence</button>
+                    <button>soirée</button>
+                    <button>voyage</button>
+                </div>
+                <div>
+                    <button>séminaire</button>
+                    <button>team building</button>
+                    <button>réunion</button>
+                </div>
+            </div>
+        </div>
+
+    </article>-->
+
+</main>
+
+<footer id="footer">
+    <h2>Ceci est un footer</h2>
+
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/debug.addIndicators.min.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/animation.gsap.js'></script>
+<script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+<script>
+    var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 48.88821, lng: 2.168432100000018},
+            zoom: 16,
+            scrollwheel: false,
+            navigationControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            draggable: false,
+            styles: [
+                {
+                    featureType: "poi.business",
+                    elemntType:  "label",
+                    stylers:
+                        [
+                            {
+                                visibility: "off"
+                            }
+                        ]
+                }
+            ]
+        });
+
+        var agence = {lat: 48.88821, lng: 2.168432100000018};
+        var marker = new google.maps.Marker({position: agence, map: map});
+
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Watt Events</h1>'+
+            '<div id="bodyContent">'+
+            '<p>21 avenue Edouard Belin <br>' +
+            '92500 - Rueil-Malmaison</p>'+
+            'Tel : 01 58 74 80 15</p>'+
+            '</div>'+
+            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+    }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPwQELALH9MW0Ir9JfK6O_oWcz1WjClYg&callback=initMap"
+        async defer></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+<script src="js/mail.js"></script>
+
+
+</body>
+
+</html>
